f81cc70000e642ac0acc7a0da9fabb25970aba29
William Elí O.L <ramun346416@gmail.com>
2023-08-15 02:17:53 +0000

    Update data1.h
    Se hace la correcta distribucion de la pagina web con su JS, para obtener una funcion por cada boton.

--- data1.h
+++ data1.h
@@ -3,17 +3,34 @@
 // asi ya no se tiene que hacer un nuevo objeto .
 // En otras palabras, la función puede modificar el objeto original y los cambios se 
 // reflejarán fuera de la función.
-void web(WiFiClient &client){
+void web(WiFiClient &cliente){
+            cliente.println("HTTP/1.1 200 OK");
+            cliente.println("Content-type:text/html");
+            cliente.println();
+            cliente.print(pagina);
    for (int n=1; n<7; n++)
             {
-            client.print(pagina1);
-            client.print(n);
-            client.print(pagina2);
-            client.print(n);
-            client.print(pagina3);
-            client.print(n);
-            client.print(pagina4);
+            cliente.print(pagina1);
+            cliente.print(n);
+            cliente.print(pagina2);
+            cliente.print(n);
+            cliente.print(pagina3);
+            cliente.print(n);
+            cliente.print(pagina4);
             }
-            client.print(pagina6);
+            cliente.print(pagina5);
+   for (int i=1; i<7; i++){
+            //cliente.print(i);
+            cliente.print(pagina6);
+            cliente.print(i);
+            cliente.print(pagina7);
+            cliente.print(i);
+            cliente.print(pagina8);
+            cliente.print(i);
+            cliente.print(pagina9);
+            cliente.print(i);
+            cliente.print(pagina10);
+            }
+            cliente.print(pagina11);
   }
   
